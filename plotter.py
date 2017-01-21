1352558f849b868f0c59f11b75e5d2f0ddce4855
Willian Galvani <williangalvani@gmail.com>
2017-01-21 17:41:29 -0200

    add static image generation
    Signed-off-by: Willian Galvani <williangalvani@gmail.com>

--- plotter.py
+++ plotter.py
@@ -1,13 +1,14 @@
 import matplotlib.pyplot as plt
 from matplotlib.colors import BoundaryNorm
 from matplotlib.ticker import MaxNLocator
-import numpy as np
 import matplotlib.animation as animation
-
+import matplotlib.colors as colors
+import matplotlib.cm as cmx
+import numpy as np
 
 class PlotFunctionAndTriangle:
 
-    def __init__(self, function, datapoints):
+    def __init__(self, function, datapoints, animated=False):
 
         self.dataPoints = datapoints
         dx, dy = 0.05, 0.05
@@ -21,7 +22,7 @@ class PlotFunctionAndTriangle:
         # x and y are bounds, so z should be the value *inside* those bounds.
         # Therefore, remove the last value from the z array.
         z = z[:-1, :-1]
-        levels = MaxNLocator(nbins=15).tick_values(z.min(), z.max())
+        levels = MaxNLocator(nbins=150).tick_values(z.min(), z.max())
 
         # pick the desired colormap, sensible levels, and define a normalization
         # instance which takes data values and translates those into levels.
@@ -38,19 +39,34 @@ class PlotFunctionAndTriangle:
         self.fig.colorbar(cf, ax=ax1)
         ax1.set_title('Nelder-Mead on a parabolic function')
 
-        initial = [[0, 1, 2], [0, 1, 2]]
-        self.line, = ax1.plot(*initial)
 
         # adjust spacing between subplots so `ax1` title and `ax0` tick labels
         # don't overlap
         self.fig.tight_layout()
 
-        ani = animation.FuncAnimation(self.fig, self.animate, len(self.dataPoints),
-                                      interval=300, blit=True)
-        ani.save('animation.gif', writer='imagemagick', fps=3)
+        if animated:
+            initial = [[0, 1, 2], [0, 1, 2]]
+            self.line, = ax1.plot(*initial)
+
+            ani = animation.FuncAnimation(self.fig, self.animate, len(self.dataPoints),
+                                          interval=300, blit=True)
+            #ani.save('animation.gif', writer='imagemagick', fps=3)
+        else:
+
+            jet = plt.get_cmap('jet')
+            cNorm = colors.Normalize(vmin=0, vmax=len(self.dataPoints))
+            scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=jet)
+            print(scalarMap.get_clim())
+
+            for i, points in enumerate(self.dataPoints):
+                colorVal = scalarMap.to_rgba(i)
+                points = np.append(points, [points[0]], axis=0)
+                print(points.transpose())
+                ax1.plot(*points.transpose(),color=colorVal)
         plt.show()
 
     def animate(self, i):
         points = np.append(self.dataPoints[i], [self.dataPoints[i][0]], axis=0)
         self.line.set_data(points.transpose())  # update the data
         return self.line,
+
